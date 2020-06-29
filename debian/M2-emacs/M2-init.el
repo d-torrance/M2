e8c8ee56f2e0bacf04808bafd056053b60d89482
Doug Torrance <dtorrance@piedmont.edu>
2020-06-28 21:47:06 -0400

    Add contents of M2-emacs git submodule to debian directory
    We copy it to the expected place during dh_auto_configure, right before
    we run ./configure, which is when the submodule is updated in
    upstream's build script.

---
+++ debian/M2-emacs/M2-init.el
@@ -0,0 +1,10 @@
+;; Setup M2.el for autoloading
+
+(autoload 'M2             "M2" "Run Macaulay2 in an emacs buffer" t)
+(autoload 'M2-mode        "M2" "Macaulay2 editing mode" t)
+(autoload 'M2-comint-mode "M2" "Macaulay2 command interpreter mode" t)
+(add-to-list 'auto-mode-alist '("\\.m2\\'" . M2-mode))
+
+;; Uncomment these lines to enable syntax highlighting for the interpreter language
+;(autoload 'D-mode "D-mode" "Editing mode for the interpreter language" t)
+;(add-to-list 'auto-mode-alist '("\\.dd?\\'" . D-mode))
