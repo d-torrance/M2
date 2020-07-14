719582f454f9f354d528411bb1299089f4f52d5f
Doug Torrance <dtorrance@piedmont.edu>
2020-07-14 07:20:58 -0400

    Update M2-emacs submodule before build

--- M2/BUILD/doug/build.sh
+++ M2/BUILD/doug/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git submodule init ../../Macaulay2/editors/emacs
+git submodule update
 make -C ../..
 ../../configure --prefix=/usr --disable-shared \
  --with-gtest-source-path=/home/profzoom/src/googletest/googletest/googletest
