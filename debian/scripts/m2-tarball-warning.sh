a130b9a31bc6d5c783f8146a92aa37834baf870d
Doug Torrance <dtorrance@piedmont.edu>
2020-12-31 17:20:57 -0500

    debian/scripts/m2-tarball-warning.sh: New script; displays warning message if user tries to generate a tarball with uscan.

---
+++ debian/scripts/m2-tarball-warning.sh
@@ -0,0 +1,4 @@
+>&2 echo "+-----------------------------------------------------------------+"
+>&2 echo "| Do NOT use this tarball.  It is missing files from the M2-emacs |"
+>&2 echo "| submodule.  Instead, run 'debian/rules get-orig-source'.        |"
+>&2 echo "+-----------------------------------------------------------------+"
