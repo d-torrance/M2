42020b6b06f2dd38b3ffa08a9aa3019972289968
Doug Torrance <dtorrance@piedmont.edu>
2021-01-22 15:19:39 -0500

    PPA update script improvements -- don't suppress error output

--- debian/scripts/m2-update-ppa-branches.sh
+++ debian/scripts/m2-update-ppa-branches.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
 
+set -e
+
 CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
 if [ $CURRENT_BRANCH != "debian/development" ]
 then
     echo -n "checking out debian/development ... "
-    git checkout debian/development 2> /dev/null
+    git checkout -q debian/development
     echo "done"
 fi
 
 echo -n "pushing debian/development to salsa ... "
-git push salsa debian/development 2> /dev/null
+git push -q salsa debian/development
 echo "done"
 
 echo -n "checking out ppa/bionic ... "
-git checkout ppa/bionic 2> /dev/null
+git checkout -q ppa/bionic
 echo "done"
 
 echo -n "merging debian/development into ppa/bionic ... "
-git merge --no-edit debian/development > /dev/null
+git merge -q --no-edit debian/development
 echo "done"
 
 echo -n "pushing ppa/bionic to salsa ... "
-git push salsa ppa/bionic 2> /dev/null
+git push -q salsa ppa/bionic
 echo "done"
 
 echo -n "checking out debian/development ... "
-git checkout debian/development > /dev/null 2>&1
+git checkout -q debian/development
 echo "done"
