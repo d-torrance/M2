e8126d5c116d2c24c334b0c1cfe2b63dae0f7bc2
Doug Torrance <dtorrance@piedmont.edu>
2021-11-12 10:26:09 -0500

    d/scripts/m2-get-orig-source.sh: Only merge if needed.

--- debian/scripts/m2-get-orig-source.sh
+++ debian/scripts/m2-get-orig-source.sh
@@ -149,40 +149,46 @@ fi
 
 if [ $MERGE ]
 then
-    echo "merging '$REF' ..."
-    git merge --no-edit FETCH_HEAD
-
-    echo -n "refreshing patches ... "
-
-    REFRESH_PATCHES=
-    quilt pop -a > /dev/null 2>&1 || true
-
-    while true
-    do
-	QUILT_PUSH=$(quilt push 2> /dev/null || true)
-	if echo $QUILT_PUSH | grep "does not apply" > /dev/null
-	then
-	    echo "\ncan't apply patch; refresh manually"
-	    exit 1
-	elif echo $QUILT_PUSH | grep offset > /dev/null
-	then
-	    quilt refresh
-	    REFRESH_PATCHES=1
-	elif [ -z "$QUILT_PUSH" ]
-	then
-	    break
-	fi
-    done
-
-    quilt pop -a > /dev/null 2>&1 || true
-
-    if [ $REFRESH_PATCHES ]
+    echo -n "merging '$REF' ... "
+    if git merge-base --is-ancestor FETCH_HEAD HEAD
     then
-	echo "done"
-	git add debian/patches
-	git commit -m "Refresh patches for $VERSION"
-    else
 	echo "not needed"
+    else
+	echo
+	git merge --no-edit FETCH_HEAD
+
+	echo -n "refreshing patches ... "
+
+	REFRESH_PATCHES=
+	quilt pop -a > /dev/null 2>&1 || true
+
+	while true
+	do
+	    QUILT_PUSH=$(quilt push 2> /dev/null || true)
+	    if echo $QUILT_PUSH | grep "does not apply" > /dev/null
+	    then
+		echo "\ncan't apply patch; refresh manually"
+		exit 1
+	    elif echo $QUILT_PUSH | grep offset > /dev/null
+	    then
+		quilt refresh
+		REFRESH_PATCHES=1
+	    elif [ -z "$QUILT_PUSH" ]
+	    then
+		break
+	    fi
+	done
+
+	quilt pop -a > /dev/null 2>&1 || true
+
+	if [ $REFRESH_PATCHES ]
+	then
+	    echo "done"
+	    git add debian/patches
+	    git commit -m "Refresh patches for $VERSION"
+	else
+	    echo "not needed"
+	fi
     fi
 fi
 
