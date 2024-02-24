4f62aee1aeeb0af9e244c1e9c11e5e4543163f25
Dave Barton <dbarton@mathscribe.com>
2024-02-23 17:12:05 -0800

    Cure data race bugs
    Fix several bugs found by ThreadSanitizer, by making several variables
    threadLocal, and by making threadFramesize and emit_line_len each be
    (essentially) an atomic_int.

--- M2/Macaulay2/d/atomic.d
+++ M2/Macaulay2/d/atomic.d
@@ -4,6 +4,7 @@ declarations "
   #include \"M2/atomic-field.h\"
   #ifdef __cplusplus
     #include <atomic>
+    using std::atomic_fetch_add;
     using std::atomic_signal_fence;
     using std::memory_order_seq_cst;
   #else
@@ -14,7 +15,10 @@ declarations "
 export atomicField := Type "struct atomic_field";
 export load(x:atomicField) ::= Ccode(int, "load_Field(",x,")");
 export test(x:atomicField) ::= (load(x) != 0);
-export store(x:atomicField,y:bool) ::= Ccode(void, "store_Field(",x,",",y,")");
+export store(x:atomicField,y:int) ::= Ccode(void, "store_Field(",x,",",y,")");
+export store(x:atomicField,y:bool) ::= store(x, Ccode(int, y));
+export fetch_add(x:atomicField,y:int) ::= Ccode(int, "atomic_fetch_add(&(",x,").field,",y,")");
+
 export compilerBarrier() ::= Ccode(void,"atomic_signal_fence(memory_order_seq_cst)");
 
 -- Local Variables:
