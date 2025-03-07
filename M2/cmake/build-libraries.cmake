58a6ba3bac1a9c4971d72207a4159d78b7076ef9
Doug Torrance <dtorrance@piedmont.edu>
2025-03-07 07:49:18 -0500

    Bump nauty to version 2.8.9

--- M2/cmake/build-libraries.cmake
+++ M2/cmake/build-libraries.cmake
@@ -929,8 +929,8 @@ set(nauty_BINARIES
   genspecialg gentourng gentreeg hamheuristic labelg linegraphg listg multig newedgeg pickg
   planarg ranlabg shortg showg subdivideg twohamg vcolg watercluster2)
 ExternalProject_Add(build-nauty
-  URL               https://pallini.di.uniroma1.it/nauty2_8_8.tar.gz
-  URL_HASH          SHA256=159d2156810a6bb240410cd61eb641add85088d9f15c888cdaa37b8681f929ce
+  URL               https://pallini.di.uniroma1.it/nauty2_8_9.tar.gz
+  URL_HASH          SHA256=c97ab42bf48796a86a598bce3e9269047ca2b32c14fc23e07208a244fe52c4ee
   PREFIX            libraries/nauty
   SOURCE_DIR        libraries/nauty/build
   DOWNLOAD_DIR      ${CMAKE_SOURCE_DIR}/BUILD/tarfiles
